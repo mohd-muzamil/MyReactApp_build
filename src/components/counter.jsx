0b6adce296507ad5315116c315be5c79a576d2f9
Mohamed-Muzamil-H <mohd.muzamil.08@gmail.com>
2020-09-07 14:59:36 -0300

    V1

---
+++ src/components/counter.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.counter.value,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  handleIncrement = () => {
+    // console.log("Increment Clicked", this.state.value);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    // console.log("props", this.props);
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn=sm"
+        >
+          Increment
+        </button>
+
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+
+        {/* <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul> */}
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    return this.state.value === 0 ? "Zero" : this.state.value;
+  }
+}
+
+export default Counter;
